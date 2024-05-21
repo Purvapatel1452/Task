47d9952ef3afcd00dc3a6783b857b03709e2ace1
purva <purva@gmail.com>
2024-05-21 16:42:56 +0530

    commit for payment route updation in backend

--- Backend/routes/payment-routes.js
+++ Backend/routes/payment-routes.js
@@ -24,39 +24,39 @@ paymentRouter.post('/create-payment-intent', async (req, res) => {
   }
 })
 
-paymentRouter.post('/payee',async(req,res)=>{
-  try{
-    console.log('hi payee')
-    console.log(req.body.totalAmount)
-    const totalAmount=100
-    console.log(totalAmount)
-    if(!totalAmount){
-      return res.status(404).send({
-        success:false,
-        message:'Total Amount is require'
-      })
-    }
-    console.log("22")
-    const client_secret= await stripe.paymentIntents.create({
-      amount:Number(totalAmount),
-      currency:'usd',
+// paymentRouter.post('/payee',async(req,res)=>{
+//   try{
+//     console.log('hi payee')
+//     console.log(req.body.totalAmount)
+//     const totalAmount=100
+//     console.log(totalAmount)
+//     if(!totalAmount){
+//       return res.status(404).send({
+//         success:false,
+//         message:'Total Amount is require'
+//       })
+//     }
+//     console.log("22")
+//     const client_secret= await stripe.paymentIntents.create({
+//       amount:Number(totalAmount),
+//       currency:'usd',
      
-    })
-    console.log("22",client_secret)
-    res.status(200).send({
-      success:true,
-      client_secret
-    })
+//     })
+//     console.log("22",client_secret)
+//     res.status(200).send({
+//       success:true,
+//       client_secret
+//     })
 
-  }catch(error){
-    console.log(error);
-    res.status(500).send({
-      success:false,
-      message:"Error In Get UPDATE Products API",
-      error,
-    })
-  }
-})
+//   }catch(error){
+//     console.log(error);
+//     res.status(500).send({
+//       success:false,
+//       message:"Error In Get UPDATE Products API",
+//       error,
+//     })
+//   }
+// })
 
 
 
@@ -87,27 +87,52 @@ catch (e) {
       error: e.message,
     });
 }
-
-
 })
 
 paymentRouter.post('/stripe/webhook', async (req, res) => {
   console.log("WEBHOOK")
   const event = req.body;
+  console.log(event.type)
+  
   console.log("RR",event.data.object)
 
   // Handle the payment_intent.succeeded event
   if (event.type === 'payment_intent.succeeded') {
+    console.log("IF")
       const paymentIntent = event.data.object;
-      console.log("RRee",event.data.object.amount)
+      console.log("RRee",event.data.object)
       
       // Store payment data in your database
       // Example: Save paymentIntent.id or any other necessary data
       
       console.log('PaymentIntent was successful',paymentIntent);
   }
+  if (event.type === 'charge.succeeded') {
+    console.log("Charge")
+    responsePayload.redirectUrl=event.data.object.receipt_url
+  }
   console.log("TT",event.data.object.amount)
   res.json({ received: true,data:event.data });
+});
+
+paymentRouter.post('/upi-webhook', async(req,res)=>{
+
+  console.log('WEBHOO')
+
+  const event=req.body;
+  console.log('EVENt',event,"+++");
+
+  if(event.status==='SUCCESS'){
+    console.log('Payment was successfull',event)
+  }
+
+  else{
+    console.log('Payment failed', event);
+
+  }
+
+  res.json({received:true});
+  
 });
 
 
